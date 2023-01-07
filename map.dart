50629ddbb8727b069c3c21e3cb4a12333a5c8bdc
akmhmgc <akimuhamaguchi@icloud.com>
2023-01-07 14:08:13 +0900

    Mapのキーと値の型指定

--- map.dart
+++ map.dart
@@ -1,5 +1,5 @@
 void main(List<String> args) {
-  var map = {'A': 1, 'B': 2, 'C': 3};
+  var map = <String, int>{'A': 1, 'B': 2, 'C': 3};
   map["D"] = map['A']! + (map['B'] ?? 0);
   print(map);
 }
