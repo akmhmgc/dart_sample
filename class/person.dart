1d747b3d146cec0faf58cf7cf9d0a622849694ef
akmhmgc <akimuhamaguchi@icloud.com>
2023-01-07 15:13:56 +0900

    コンストラクタの使用

--- class/person.dart
+++ class/person.dart
@@ -1,13 +1,14 @@
 void main(List<String> args) {
-  Person me = Person();
-  me.name = '太郎';
-  me.age = 20;
+  Person me = Person('太郎', 30);
   me.greeting();
 }
 
 class Person {
   String name = '';
   int age = 0;
+  // コンストラクタ
+  Person(this.name, this.age);
+
   void greeting() {
     print("私は$nameです。年齢は$ageです");
   }
