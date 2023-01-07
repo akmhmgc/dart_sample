4b0a8adb34b7647d006e554d4145416122b5f7ac
akmhmgc <akimuhamaguchi@icloud.com>
2023-01-07 14:40:43 +0900

    戻り値の型を指定しない関数

--- methods/sample.dart
+++ methods/sample.dart
@@ -1,7 +1,14 @@
 void main(List<String> args) {
   print(hello());
+  print(goodbye());
 }
 
+// 関数の前に戻り値を指定する
 String hello(){
   return 'こんにちは';
+}
+
+// 戻り値を指定しなくても良い
+goodbye(){
+  return 'さようなら';
 }
