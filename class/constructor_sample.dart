8f0dfc152089fad993cc8ae8941c0d1a538fe7a4
akmhmgc <akimuhamaguchi@icloud.com>
2023-05-13 21:17:37 +0900

    コンストラクタ

---
+++ class/constructor_sample.dart
@@ -0,0 +1,25 @@
+class Person {
+  late String _firstName;
+  late String lastName;
+
+  Person(this._firstName, this.lastName);
+  // 省略しない場合は以下のように書く
+  // Person(String firstName, String lastName) {
+  //   this.firstName = firstName ;
+  //   this.lastName = lastName;
+  // }
+
+  void greeting() {
+    print("私は${lastName}${_firstName}です");
+  }
+}
+
+void main() {
+  Person person = Person("太郎", "山田");
+
+  print(person.lastName);
+  // アンダースコアで始まる変数はprivateになるので、以下のようにはアクセスできない
+  // print(person._firstName);
+
+  person.greeting();
+}
